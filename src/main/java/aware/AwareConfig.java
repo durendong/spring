51283f941dc7bfe4a5f1c6b0f025164fd82caab0
durendong@126.com <du63477811>
2017-01-16 23:44:13 +0800

    Spring Aware

---
+++ src/main/java/aware/AwareConfig.java
@@ -0,0 +1,12 @@
+package aware;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by durendong on 2017/1/16.
+ */
+@Configuration
+@ComponentScan("aware")
+public class AwareConfig {
+}
